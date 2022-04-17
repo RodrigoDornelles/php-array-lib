de575b99959292edb6594c29cc883811a12461c8
RodrigoDornelles <rodrigo@dornelles.me>
2022-04-17 09:33:13 -0300

    add readme examples

---
+++ examples/readme-functional-libary.php
@@ -0,0 +1,12 @@
+<?php 
+
+/** works without composer */
+require_once __DIR__.'/../src/ArrayCreate.php';
+
+$numbers = [54, 32, 15, 3, 7, 42];
+
+echo ArrayCreate::from($numbers)
+    ->sort()
+    ->filter(fn($n) => !($n % 3))
+    ->map(fn($n) => $n * 2)
+    ->join(', ');
---
+++ examples/readme-functional-native.php
@@ -0,0 +1,13 @@
+<?php
+
+$numbers = [54, 32, 15, 3, 7, 42];
+
+sort($numbers);
+
+echo implode(', ',
+    array_map(fn($n) => $n * 2,
+        array_filter($numbers,
+            fn($n) => !($n % 3)
+        )
+    )
+);
